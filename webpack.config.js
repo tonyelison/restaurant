ab864adaea1dedea7fa84d24637aa928ac523958
Anthony Elison <tonyelison37@gmail.com>
2023-05-04 13:08:21 -0600

    Change output js to use 'bundle' namespace

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     runtimeChunk: 'single',
   },
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
