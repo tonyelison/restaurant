b73914d1c85833c1c42b1b6a12a0790c6518d965
Anthony Elison <tonyelison37@gmail.com>
2023-05-04 15:28:12 -0600

    Render nav and h1 via page-load component

--- src/page-load.js
+++ src/page-load.js
@@ -1,5 +1,33 @@
+const renderNav = () => {
+  const nav = document.createElement('nav');
+  const ul = document.createElement('ul');
+
+  ['Home', 'Menu', 'Contact'].forEach((tabName) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    a.textContent = tabName;
+
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+
+  nav.appendChild(ul);
+  return nav;
+};
+
+const renderHeader = () => {
+  const h1 = document.createElement('h1');
+
+  h1.textContent = 'Lorem Ipsum';
+
+  return h1;
+};
+
 const pageLoad = () => {
-  console.log('page-load');
+  const content = document.querySelector('.content');
+  content.appendChild(renderNav());
+  content.appendChild(renderHeader());
 };
 
 export default pageLoad;
