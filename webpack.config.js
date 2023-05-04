3e6cc590b843354e9e14353be3b07c2a3af5f7d0
Anthony Elison <tonyelison37@gmail.com>
2023-05-04 12:28:30 -0600

    Add HTMLWebpackPlugin, webpack config, run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Homepage',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -13,5 +19,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
