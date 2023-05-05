bd1fd8fed608583cc97aee8e98f6c6d09c92b92a
Anthony Elison <tonyelison37@gmail.com>
2023-05-05 13:00:22 -0600

    Update nav to call page render logic on link click

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,27 @@
+import renderHome from './home';
+
 const renderNav = () => {
   const nav = document.createElement('nav');
   const ul = document.createElement('ul');
 
-  ['Home', 'Menu', 'Contact'].forEach((tabName) => {
+  const tabs = [
+    { name: 'Home', render: renderHome },
+    { name: 'Menu', render: renderHome },
+    { name: 'Contact', render: renderHome },
+  ];
+
+  tabs.forEach((tab) => {
     const li = document.createElement('li');
     const a = document.createElement('a');
 
-    a.textContent = tabName;
-    a.href = '';
+    a.textContent = tab.name;
+    a.addEventListener('click', () => {
+      const pageContent = document.querySelector('.page-content');
+      if (!pageContent.classList.contains(tab.name.toLowerCase())) {
+        const contentContainer = document.getElementById('content');
+        contentContainer.replaceChild(tab.render(), pageContent);
+      }
+    });
 
     li.appendChild(a);
     ul.appendChild(li);
